c89d9a8541278a4d4395bccef396260ccf950d32
Abhinav Jonnada <abhinav.jonnada@gmail.com>
2023-08-31 00:46:47 -0400

    integrated venmo

--- functions/shared.js
+++ functions/shared.js
@@ -1,6 +1,17 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const db = admin.firestore();
+const { validatePaymentPayload } = require('./paymentServer/schema');
+const { ApiError, client: square } = require('./paymentServer/square');
+const { nanoid } = require('nanoid');
+
+const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID
+const PAYPAL_APP_SECRET = process.env.PAYPAL_APP_SECRET
+const fetch = require("node-fetch");
+const baseURL = {
+	    sandbox: "https://api-m.sandbox.paypal.com",
+	   // production: "https://api-m.paypal.com"
+	};
 
 const setHeaders = (res) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -70,6 +81,106 @@ const addTeamData = async (data, userId) => {
        console.error(error);
        return new Error(error);
    }
+}
+
+const processCashAppPayment = async (req, res) => {
+    const idToken = req.headers.authorization?.split('Bearer ')[1];
+    const payload = {
+      locationId: req.body.locationId,
+      sourceId: req.body.sourceId
+    }
+    if (!validatePaymentPayload(payload)) { // schema validates incoming requests
+        return res.status(400).json(getResponseJSON('Bad Request!', 400));
+    }
+    try{
+    const idempotencyKey = nanoid();
+    const payment = {
+      idempotencyKey,
+      locationId: payload.locationId,
+      sourceId: payload.sourceId,
+      amountMoney: {
+        amount: req.body.payment,
+        currency: 'USD',
+      },
+    };
+    if (payload.customerId) {
+      payment.customerId = payload.customerId;
+    }
+    const { result, statusCode } = await square.paymentsApi.createPayment(payment); // square provides the API client and error types
+    if(statusCode === 200) {
+        const decodedToken = await validateIDToken(idToken);
+        await updatePaymentSuccess(decodedToken.uid);
+    }
+  }
+    catch (ex) {
+      if (ex instanceof ApiError) {
+        // likely an error in the request. don't retry
+        console.log(ex.errors);
+      } else {
+        // IDEA: send to error reporting service
+        console.log(`Error creating payment on attempt: ${ex}`);
+        throw ex; // to attempt retry
+      }
+    }
+}
+
+const createOrder = async (payment) => {
+    const accessToken = await generateAccessToken();
+    const url = `${baseURL.sandbox}/v2/checkout/orders`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: payment,
+            },
+          },
+        ],
+      }),
+    });
+    const data = await response.json();
+    return data;
+  }
+
+  	// use the orders api to capture payment for an order
+const capturePayment = async (orderId, req) => {
+ 	  const accessToken = await generateAccessToken();
+  	  const url = `${baseURL.sandbox}/v2/checkout/orders/${orderId}/capture`;
+  	  const response = await fetch(url, {
+  	    method: "POST",
+  	    headers: {
+  	      "Content-Type": "application/json",
+  	      Authorization: `Bearer ${accessToken}`,
+  	    },
+  	  });
+  	  const data = await response.json();
+      if(data.status === 'COMPLETED') {
+        const idToken = req.headers.authorization?.split('Bearer ')[1];
+        const decodedToken = await validateIDToken(idToken);
+        await updatePaymentSuccess(decodedToken.uid);
+    }
+  	  return data;
+}
+
+// generate an access token using client id and app secret
+async function generateAccessToken() {
+  const auth = Buffer.from(PAYPAL_CLIENT_ID + ":" + PAYPAL_APP_SECRET).toString("base64")
+  const response = await fetch(`${baseURL.sandbox}/v1/oauth2/token`, {
+    method: "POST",
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${auth}`,
+    },
+  });
+  const data = await response.json();
+  return data.access_token;
 }
 
 const updatePaymentSuccess = async (userId) => {
@@ -226,5 +337,8 @@ module.exports = {
     updatePaymentSuccess,
     storeAdminRules,
     grabAdminRules,
-    integrateRulesEngine
+    integrateRulesEngine,
+    processCashAppPayment,
+    createOrder,
+    capturePayment
   }
